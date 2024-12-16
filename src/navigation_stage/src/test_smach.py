cca720c53ce30244515bb543acbc65fe188ff0ba
MikuSurgebinder <maj28@alu.ua.es>
2024-12-16 18:03:29 +0100

    Estructura de la maquina de estados

--- src/navigation_stage/src/test_smach.py
+++ src/navigation_stage/src/test_smach.py
@@ -5,7 +5,6 @@ import smach_ros
 import math
 from smach import State,StateMachine
 from time import sleep
-from practica3_base import WanderAndDetect
 from volver_origen import Moverse
 
 """
@@ -20,31 +19,52 @@ ANG_IZQ = 30*math.pi/180.0
 ANG_DER = -ANG_IZQ
 """
 
-"""
-class Uno(State):
+posicion=[6]
+
+
+class Quieto(State):
+    def __init__(self):
+        State.__init__(self, outcomes=['buttonPressed','followPerson'])
+    
+    def execute(self, userdata):
+        #Velocidad=0 y pub
+        while True:
+            if 'echo en callback de boton'==True: 
+                return 'buttonPressed'
+            if 'echo en callback de seguimiento'==True:
+                return 'followPerson'
+            
+class Boton(State):
     def __init__(self):
         State.__init__(self, outcomes=['success'])
     
     def execute(self, userdata):
-        print('Deambular hasta encontrar rojo')
-        color_detected = False
-        a = WanderAndDetect()
-        while not color_detected:
-            color_detected = not a==""
-        sleep(1)
-        print('Rojo encontrado')
+        #Detectar que boton se ha pulsado y como
+        boton = Boton() #Crear clase boton con métodos e importarlo
+        while True:
+            if 'boton . tipo de presionado() '== 'largo':
+                posicion[boton.numBoton()] = getLocalizacion() #Crear handyFunciones e importarlo
+                break
+            if 'boton . tipo de presionado() '== 'corto':
+                irLocalizacion(posicion[boton.numBoton()])
+                esperarATerminar()
+                break
         return 'success'
-"""
 
-class Dos(State): #Segundo estado de la maquina
+
+
+class Seguimiento(State): #Segundo estado de la maquina
     def __init__(self):
-        State.__init__(self, outcomes=['end'])
+        State.__init__(self, outcomes=['success'])
     
     def execute(self, userdata):
-        print('Volver a casa')
-        Moverse(([5,4],[0,0,0,1])) #Volver a casa, coordenadas especificadas a mano, código en volver_origen.py
-        sleep(1)
-        return 'end' #Finaliza la maquina de estados
+        bandera = True
+        while bandera:
+            dir = vision() #Importar de Erika
+            seguimiento(dir) #Importar de Gemma
+            if CambioDeOrden:
+                return 'success'
+
 
 
 
@@ -54,8 +74,9 @@ if __name__ == '__main__':
     #Maquina de estados
     sm = StateMachine(outcomes=['stop'])
     with sm:
-        StateMachine.add('WanderUntilRed', WanderAndDetect(), transitions={'success':'ACasita'}) #Deambular hasta encontar rojo, en practica3_base.py
-        StateMachine.add('ACasita', Dos(), transitions={'end':'stop'}) #Navegación de volver al origen
+        StateMachine.add('WaitingForOrders', Quieto(), transitions={'buttonPressed':'ButtonCommand','followPerson':'FollowCommand'})
+        StateMachine.add('ButtonCommand', Boton(), transitions={'success':'WaitingForOrders'})
+        StateMachine.add('FollowCommand', Seguimiento(), transitions={'success':'WaitingForOrders'})
     
     #"sis" solo sirve para mostrar una representación gráfica de la máquina de estados    
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
