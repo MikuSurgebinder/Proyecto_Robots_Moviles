cbb077762c835299ac368fad3bf14b2561c81deb
MikuSurgebinder <maj28@alu.ua.es>
2024-12-16 20:13:37 +0100

    Nueva rama para trabajar en la implementacion de vision

--- src/navigation_stage/src/test_smach.py
+++ src/navigation_stage/src/test_smach.py
@@ -6,6 +6,7 @@ import math
 from smach import State,StateMachine
 from time import sleep
 from volver_origen import Moverse
+from vision import TurtleCamProcessor
 
 """
 #Topics a usar en suscripciones y publicaciones
@@ -19,26 +20,34 @@ ANG_IZQ = 30*math.pi/180.0
 ANG_DER = -ANG_IZQ
 """
 
-posicion=[6]
+posicion=[6] #Esturcura de posicion: es un vector con 6 posiciones almacenadas:
+             #Cada posicion contiene una matriz, una fila con 3 coord de posicion (x,y,z)
+             #y otra con 4 coord de orientacion (x,y,z,w)
 
 
-class Quieto(State):
+class Static(State):
     def __init__(self):
-        State.__init__(self, outcomes=['buttonPressed','followPerson'])
+        State.__init__(self, outcomes=['buttonPressed','followPerson','mapping'])
     
     def execute(self, userdata):
         #Velocidad=0 y pub
+
+        print("Estado parado")
+        return 'followPerson'
         while True:
             if 'echo en callback de boton'==True: 
                 return 'buttonPressed'
             if 'echo en callback de seguimiento'==True:
                 return 'followPerson'
+            if 'echo en callback de mapeado'==True:
+                return 'mapping'
             
-class Boton(State):
+class Button(State):
     def __init__(self):
         State.__init__(self, outcomes=['success'])
     
     def execute(self, userdata):
+        """
         #Detectar que boton se ha pulsado y como
         boton = Boton() #Crear clase boton con métodos e importarlo
         while True:
@@ -46,24 +55,43 @@ class Boton(State):
                 posicion[boton.numBoton()] = getLocalizacion() #Crear handyFunciones e importarlo
                 break
             if 'boton . tipo de presionado() '== 'corto':
-                irLocalizacion(posicion[boton.numBoton()])
-                esperarATerminar()
+                Moverse(posicion[boton.numBoton()])
                 break
+        """
         return 'success'
+        
 
 
 
-class Seguimiento(State): #Segundo estado de la maquina
+class Follow(State): #Segundo estado de la maquina
     def __init__(self):
         State.__init__(self, outcomes=['success'])
     
     def execute(self, userdata):
-        bandera = True
-        while bandera:
-            dir = vision() #Importar de Erika
-            seguimiento(dir) #Importar de Gemma
-            if CambioDeOrden:
-                return 'success'
+        bandera = False
+        processor = TurtleCamProcessor()
+        rospy.sleep(0.1)
+        print("Estado camara seguimiento")
+        try:
+            rate = rospy.Rate(10)  # Procesa frames a 10 Hz
+            while not rospy.is_shutdown():
+                processor.process_frame()
+                dir = [processor.area,processor.x_coord,processor.y_coord]
+                print(dir)
+                rate.sleep()
+        except bandera: #rospy.ROSInterruptException or 
+            pass
+        finally:
+            processor.cleanup()
+
+
+class Map(State): #Segundo estado de la maquina
+    def __init__(self):
+        State.__init__(self, outcomes=['success'])
+    
+    def execute(self, userdata):
+        #Llamar a mapeado con deambulacion
+            return 'success'
 
 
 
@@ -71,12 +99,15 @@ class Seguimiento(State): #Segundo estado de la maquina
 if __name__ == '__main__':
     rospy.init_node("test_smach")
 
+    #En el arranque, añadir un lector de datos para las localizaciones guardadas previamente
+
     #Maquina de estados
     sm = StateMachine(outcomes=['stop'])
     with sm:
-        StateMachine.add('WaitingForOrders', Quieto(), transitions={'buttonPressed':'ButtonCommand','followPerson':'FollowCommand'})
-        StateMachine.add('ButtonCommand', Boton(), transitions={'success':'WaitingForOrders'})
-        StateMachine.add('FollowCommand', Seguimiento(), transitions={'success':'WaitingForOrders'})
+        StateMachine.add('WaitingForOrders', Static(), transitions={'buttonPressed':'ButtonCommand','followPerson':'FollowCommand','mapping':'MapCommand'})
+        StateMachine.add('ButtonCommand', Button(), transitions={'success':'WaitingForOrders'})
+        StateMachine.add('FollowCommand', Follow(), transitions={'success':'WaitingForOrders'})
+        StateMachine.add('MapCommand', Map(), transitions={'success':'WaitingForOrders'})
     
     #"sis" solo sirve para mostrar una representación gráfica de la máquina de estados    
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
